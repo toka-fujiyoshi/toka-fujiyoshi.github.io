2508380c1de735a6ff014d6dd2d14cca79f2aa8b
toka-fujiyoshi <c82-fuyuki@hotmail.co.jp>
2025-01-11 19:21:29 +0900

    テスト前に実行するスクリプトのファイル名誤りを修正

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,6 @@ export default defineConfig({
     // test: {
     //     globals: true,
     //     environment: 'happy-dom',
-    //     setupFiles: './setupTests.ts',
+    //     setupFiles: './vitest-setup.ts',
     // },
 })
